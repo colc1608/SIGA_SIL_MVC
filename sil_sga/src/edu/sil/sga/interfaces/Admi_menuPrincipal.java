6ac59086c2e57b60a45314ed94d46f4068efc4e3
Cesar Lopez <Cesar Lopez@loopezDesktop>
2015-05-10 16:39:07 -0500

    arreglando el menu principal de Admi

--- sil_sga/src/edu/sil/sga/interfaces/Admi_menuPrincipal.java
+++ sil_sga/src/edu/sil/sga/interfaces/Admi_menuPrincipal.java
@@ -31,14 +31,18 @@ public class Admi_menuPrincipal extends javax.swing.JFrame {
         jdPrincipal = new javax.swing.JDesktopPane();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
+        jMenuItem7 = new javax.swing.JMenuItem();
         jMenuItem5 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         itemEspecialidad = new javax.swing.JMenuItem();
         itemDocente = new javax.swing.JMenuItem();
         jMenuItem3 = new javax.swing.JMenuItem();
-        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem1 = new javax.swing.JMenuItem();
         jMenu3 = new javax.swing.JMenu();
         jMenu4 = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem6 = new javax.swing.JMenuItem();
+        jMenuItem4 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -55,6 +59,11 @@ public class Admi_menuPrincipal extends javax.swing.JFrame {
 
         jMenu1.setText("Inicio");
 
+        jMenuItem7.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_P, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItem7.setText("Principal");
+        jMenu1.add(jMenuItem7);
+
+        jMenuItem5.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F4, java.awt.event.InputEvent.ALT_MASK));
         jMenuItem5.setText("Salir");
         jMenu1.add(jMenuItem5);
 
@@ -84,9 +93,9 @@ public class Admi_menuPrincipal extends javax.swing.JFrame {
         jMenuItem3.setText("Alumno");
         jMenu2.add(jMenuItem3);
 
-        jMenuItem4.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_P, java.awt.event.InputEvent.CTRL_MASK));
-        jMenuItem4.setText("Parentesco");
-        jMenu2.add(jMenuItem4);
+        jMenuItem1.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_C, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItem1.setText("Curso");
+        jMenu2.add(jMenuItem1);
 
         jMenuBar1.add(jMenu2);
 
@@ -94,6 +103,16 @@ public class Admi_menuPrincipal extends javax.swing.JFrame {
         jMenuBar1.add(jMenu3);
 
         jMenu4.setText("Ayuda");
+
+        jMenuItem2.setText("Contenito de Ayuda");
+        jMenu4.add(jMenuItem2);
+
+        jMenuItem6.setText("Reportar Problemas");
+        jMenu4.add(jMenuItem6);
+
+        jMenuItem4.setText("Acerca de SIL SGA");
+        jMenu4.add(jMenuItem4);
+
         jMenuBar1.add(jMenu4);
 
         setJMenuBar(jMenuBar1);
@@ -169,9 +188,13 @@ public class Admi_menuPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenu jMenu4;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JMenuItem jMenuItem7;
     private javax.swing.JDesktopPane jdPrincipal;
     // End of variables declaration//GEN-END:variables
 }
